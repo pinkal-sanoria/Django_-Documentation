af65d384598c65bed44829ea41537fffc26fb5f7
pinkal-sanoria <pinkalsanoria123@gamil.com>
2024-05-07 16:34:30 +0530

    SimpleMiddleware

---
+++ app_one/middleware.py
@@ -0,0 +1,18 @@
+
+class SimpleMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+        
+        
+    def __call__(self, request):
+        
+        print('before request')
+        
+        response = self.get_response(request)
+        
+        print('after request')
+        
+        return response
+        
+        
+        
