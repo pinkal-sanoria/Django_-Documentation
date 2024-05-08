44e02544aea91ea67d1487afd240cb35fc9108f3
pinkal-sanoria <pinkalsanoria123@gamil.com>
2024-05-08 13:13:32 +0530

    session

--- app_one/views.py
+++ app_one/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
+import time
 
-# Create your views here.
 def home(request):
-    return render(request,'app_one/app.html')
+    # Check if the session variable 'greeting' is already set
+    if 'greeting' not in request.session:
+        # Set the session variable 'greeting'
+        request.session['greeting'] = 'Hello, Django Sessions!'
+    else:
+        # Check if the session has timed out
+        if 'timeout' not in request.session:
+            # Set the session timeout (15 seconds)
+            request.session['timeout'] = time.time() + 10
+        elif time.time() > request.session['timeout']:
+            # If session has timed out, remove the 'greeting' session variable
+            del request.session['greeting']
+            # Clear the session timeout
+            del request.session['timeout']
+            # Render a different template to indicate session timeout
+            return render(request, 'app_one/session_timeout.html')
+    
+    # Render the main template
+    return render(request, 'app_one/app.html')
